b3c8a1d53d1e1cd398e692b16deae369a951d40e
saltcute <me@mizuno.ai>
2024-07-09 15:11:41 -0400

    feat: add description

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import apexSearchCommand from "./commands/search";
 import apexConnectCommand from "./commands/connect";
 
 export default class ApexMenu extends BaseMenu {
+    description = "Apex 英雄战绩查询";
+
     constructor(name: string = "apex") {
         super();
         this.name = name;
